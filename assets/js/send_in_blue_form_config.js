6a199c4172de875d7224cea69bf04a920a679d05
Lucas Bento-Versace <lucas.bentoversace@ynov.com>
2023-05-15 14:53:25 +0200

    fix modal form angular kit captcha

--- assets/js/send_in_blue_form_config.js
+++ assets/js/send_in_blue_form_config.js
@@ -6,16 +6,11 @@ window.REQUIRED_ERROR_MESSAGE = "Ce champ ne peut pas être vide. ";
 
 window.GENERIC_INVALID_MESSAGE = "Les informations fournies sont invalides. Veuillez vérifier le format du champ et réessayer.";
 
-window.translation = {
-  common: {
-    selectedList: '{quantity} list selected',
-    selectedLists: '{quantity} lists selected'
-  }
-};
+  window.translation = {
+    common: {
+      selectedList: '{quantity} list selected',
+      selectedLists: '{quantity} lists selected'
+    }
+  };
 
-var AUTOHIDE = Boolean(0);
-
-function handleCaptchaResponse() {
-  var event = new Event('captchaChange');
-  document.getElementById('sib-captcha').dispatchEvent(event);
-}
+  var AUTOHIDE = Boolean(0);
